5a691de28d7dc08040214a259a31ae42d91ed5c7
HO PENG HOU <penghou.ho@surialabs.com>
2021-09-30 22:39:23 +0800

    Fixed failed ALLOWED_HOSTS

--- officeslotbooking/settings.py
+++ officeslotbooking/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-3zbnivcn^v2jzlvisl2@sa=3f*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = [os.getenv('DJANGO_HOST', '127.0.0.1')]
+ALLOWED_HOSTS = ['officeslotbooking.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
