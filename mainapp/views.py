ffbdd90d5f7360cdb589ec25f0bbc3aae8619073
HO PENG HOU <penghou.ho@surialabs.com>
2021-10-01 14:49:55 +0800

    Installed Django REST framework
    Added Permission and Authentication

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,22 @@
-# from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import UserSerializer, GroupSerializer
 
-# Create your views here.
 
-from django.http import HttpResponse
-import datetime
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-def current_datetime(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
