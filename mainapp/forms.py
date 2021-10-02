552ddfddd26e6c6bd10b8342edbcf8d9dbf9504f
HO PENG HOU <penghou.ho@surialabs.com>
2021-10-02 16:43:49 +0800

    Check email only from @surialabs.com

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,12 +1,19 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
-
+def validate_edu_email_address(value):
+    if not value.endswith('@surialabs.com'):
+        raise forms.ValidationError("Only @surialabs.com emails allowed")
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    email = forms.EmailField(
+        max_length=254,
+        validators=[validate_edu_email_address],
+        help_text='Required. Inform a valid email address.'
+    )
 
     class Meta:
         model = User
