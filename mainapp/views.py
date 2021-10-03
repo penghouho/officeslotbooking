c9abcafd9bb5a8685d22082c445207b19f2bf1c8
HO PENG HOU <penghou.ho@surialabs.com>
2021-10-04 00:02:11 +0800

    Allowed to create and delete bookings

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,11 +5,13 @@ from rest_framework import viewsets, permissions
 from .serializers import UserSerializer, GroupSerializer, BookingSerializer
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm
 from datetime import datetime
 from django.utils.safestring import mark_safe
 from .utils import BookingCalendar
+from django.views.decorators.http import require_http_methods
+from django.http import HttpResponseRedirect
+from django.contrib import messages
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -58,14 +60,14 @@ def index(request):
         cal = cal.replace('<td ', '<td width="150" height="150"')
         cal = mark_safe(cal)
 
-        if month is 1:
+        if month == 1:
             previous_month = 12
             previous_year = year - 1
         else:
             previous_month = month - 1
             previous_year = year
 
-        if month is 12:
+        if month == 12:
             next_month = 1
             next_year = year + 1
         else:
@@ -111,12 +113,59 @@ def date_details(request):
             target_date = datetime.strptime(date_string, "%Y-%m-%d")
         except ValueError:
             target_date = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
-        print(target_date)
         bookings_from_day = Booking.objects.filter(book_date=target_date)
+        is_joined = bookings_from_day.filter(user=request.user).exists()
         context = {
-            'bookings_from_day': bookings_from_day,
+            'bookings_from_day': enumerate(bookings_from_day,start= 1),
+            'date': target_date.strftime("%Y-%m-%d"),
+            'is_joined': is_joined,
+            'reached_limit': bookings_from_day.count() >= 20,
         }
+        print(context)
         return render(request, 'date_details.html', context=context)
     else:
         return redirect('accounts/login')
 
+
+@require_http_methods(["POST"])
+def create_booking(request):
+    if request.user.is_authenticated:
+        date_string = request.POST.get('date')
+        try:
+            target_date = datetime.strptime(date_string, "%Y-%m-%d")
+        except ValueError:
+            target_date = None
+        if target_date is None:
+            return HttpResponseRedirect('/')
+        else:
+            bookings_count = Booking.objects.filter(book_date=target_date).count()
+            if bookings_count < 20:
+                booking = Booking(book_date=target_date, user=request.user)
+                try:
+                    booking.save()
+                except Exception as e:
+                    print(e)
+            return HttpResponseRedirect('/date_details/?date=%s' % request.POST.get('date'))
+    else:
+        return HttpResponseRedirect('accounts/login')
+
+
+@require_http_methods(["POST"])
+def delete_booking(request):
+    if request.user.is_authenticated:
+        date_string = request.POST.get('date')
+        try:
+            target_date = datetime.strptime(date_string, "%Y-%m-%d")
+        except ValueError:
+            target_date = None
+        if target_date is None:
+            return HttpResponseRedirect('/')
+        else:
+            booking = Booking.objects.filter(book_date=target_date).filter(user=request.user).first()
+            try:
+                booking.delete()
+            except Exception as e:
+                print(e)
+            return HttpResponseRedirect('/date_details/?date=%s' % request.POST.get('date'))
+    else:
+        return HttpResponseRedirect('accounts/login')
