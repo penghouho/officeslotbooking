f4212afcd092083b3b73f89f0a3fa23f0ecd94ac
HO PENG HOU <penghou.ho@surialabs.com>
2021-10-03 22:37:20 +0800

    Added unique index fo book_date and user for Booking model

--- mainapp/models.py
+++ mainapp/models.py
@@ -11,3 +11,8 @@ class Booking(models.Model):
     book_date = models.DateField('date booked', null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['book_date', 'user'], name='user_book_date')
+        ]
